9d03d52bc2f2555651428bc554fb909b4ceb31fa
lizasixsixsix <lizasixsixsix@gmail.com>
2018-01-30 16:49:20 +0300

    initial commit

---
+++ cooqies.googlan-filter/Controllers/GooglAnController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using cooqies.googlan_filter.Filters;
+
+namespace cooqies.googlan_filter.Controllers
+{
+    public class GooglAnController : ApiController
+    {
+        [GooglAnFilter]
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            return Ok("Google Analytics is watching you.");
+        }
+    }
+}
---
+++ cooqies.googlan-filter/Filters/GooglAnFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GoogleAnalyticsTracker.WebAPI2;
+
+namespace cooqies.googlan_filter.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method,
+        Inherited = true, AllowMultiple = true)]
+    public class GooglAnFilterAttribute : ActionTrackingAttribute
+    {
+        public GooglAnFilterAttribute() : base("UA-113333252-1")
+        { }
+    }
+}
